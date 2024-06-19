0908ce1806e0d4e740df552c63dca0b6fcab53be
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-06-19 23:09:04 +0530

    feat: middleware chaining

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,26 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      success: false,
+      message: 'Invalid Id',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next, val) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid request, name or price missing',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({ success: true, data: tours });
 };
@@ -12,12 +32,12 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   console.log(req.params);
 
-  const id = req.params.id * 1; // in the req.params, each value is in the string format, so we ahve to convert it to int
-  const tour = tours.find((el) => el.id === id);
-  // if (id > tours.length)
-  if (!tour) {
-    res.status(404).json({ success: false, message: `Invalid id: ${id}` });
-  }
+  // const id = req.params.id * 1; // in the req.params, each value is in the string format, so we ahve to convert it to int
+  // const tour = tours.find((el) => el.id === id);
+  // // if (id > tours.length)
+  // if (!tour) {
+  //   res.status(404).json({ success: false, message: `Invalid id: ${id}` });
+  // }
 
   res.status(200).json({ success: true, data: tour });
 };
@@ -25,12 +45,12 @@ exports.getTour = (req, res) => {
 exports.deleteTour = (req, res) => {
   console.log(req.params);
 
-  const id = req.params.id * 1; // in the req.params, each value is in the string format, so we ahve to convert it to int
-  const tour = tours.find((el) => el.id === id);
-  // if (id > tours.length)
-  if (!tour) {
-    res.status(404).json({ success: false, message: `Invalid id: ${id}` });
-  }
+  // const id = req.params.id * 1; // in the req.params, each value is in the string format, so we ahve to convert it to int
+  // const tour = tours.find((el) => el.id === id);
+  // // if (id > tours.length)
+  // if (!tour) {
+  //   res.status(404).json({ success: false, message: `Invalid id: ${id}` });
+  // }
 
   res.status(204).json({ success: true, data: null });
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
+
 const tourController = require('../controllers/tourController');
+
 const router = express.Router();
 
-router.route('/').get(tourController.getAllTours).post(tourController.addTour);
+// This middleware is only part of the the tour route
+// router.param('id', (req, res, next, val) => {
+//   console.log(`Tour id is: ${val}`);
+//   next();
+// });
+
+router.param('id', tourController.checkID);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.addTour); //tourController.checBody the middleware that'll be executed for the post method route
 router
   .route('/:id')
   .get(tourController.getTour)
