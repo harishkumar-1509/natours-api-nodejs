36e05fc6b70bf5cf43a0c93979958895d2895042
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-06-29 19:17:12 +0530

    feat: Add more fields for the tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,9 +5,38 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: { type: Number, required: [true, 'A tour must have a duration'] },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAvg: { type: Number, default: 4.5 },
+  ratingQuantity: { type: Number, default: 4.5 },
   price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a image cover'],
+  },
+  images: [String], // Arrays of strings
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
